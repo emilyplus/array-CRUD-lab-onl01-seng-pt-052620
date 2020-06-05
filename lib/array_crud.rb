da6da3a71c4fbbef3354749a418ab48654be2017
Emily Jones <emjones829@gmail.com>
2020-06-05 21:13:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,8 +23,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array = ["dog", "cat", "fish"]
-  array.index("dog")
+  array.index[1]
 end
 
 def retrieve_first_element_from_array(array)
