7b7ed938b8e5060d2701ecfd3e4c2933ae513616
Emily Jones <emjones829@gmail.com>
2020-06-05 20:59:05 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,23 +7,23 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array.push("Mom")
+  array.push
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  arry.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array [1]
 end
 
 def retrieve_first_element_from_array(array)
