58ddadcb40a009319d34da522ab6c2e1c5562d8d
Emily Jones <emjones829@gmail.com>
2020-06-05 20:52:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,9 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = ["Dad", "Ryan", "Emily", "Ashley"]
-  element = "Mom"
-  add_element_to_end_of_array.push(element)
+  array << "Mom"
 end
 
 def add_element_to_start_of_array(array, element)
